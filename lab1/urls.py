6b5211a75104baa87aa529b18541cf4b99661256
EsraaFarhat <esraafarhat97@gmail.com>
2021-05-18 07:52:29 +0200

    lab 3

--- lab1/urls.py
+++ lab1/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('home/', views.home),
     path('student/<st_id>', views.getStudent),
     path('all/', views.getAllStudents),
+    path('new', views.newStudent),
+    path('edit/<st_id>', views.editStudent),
+    path('delete/<st_id>', views.deleteStudent),
 ]
--- lab1/views.py
+++ lab1/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from lab1.models import Student, Track
+from lab1.forms import StudentForm, TrackForm
 # Create your views here.
 
 def home(request):
@@ -15,3 +16,32 @@ def getAllStudents(request):
     all_students = Student.objects.all()
     context = {'students': all_students}
     return render(request, 'lab1/students.html', context)
+
+def newStudent(request):
+    st_form = StudentForm()
+    if request.method == "POST":
+        st_form = StudentForm(request.POST)
+        if st_form.is_valid():
+            st_form.save()
+            return HttpResponseRedirect('/django/all')
+
+    context = {'student_form': st_form}
+    return render(request, 'lab1/new_student.html', context)
+
+def editStudent(request, st_id):
+    st = Student.objects.get(id = st_id)
+    st_form = StudentForm(instance=st)
+    if request.method == "POST":
+        st_form = StudentForm(request.POST, instance=st)
+        if st_form.is_valid():
+            st_form.save()
+            return HttpResponseRedirect('/django/all')
+
+    context = {'student_form': st_form}
+    return render(request, 'lab1/new_student.html', context)
+
+def deleteStudent(request, st_id):
+    student = Student.objects.get(id = st_id)
+    student.delete()
+    return HttpResponseRedirect('/django/all')
+    
